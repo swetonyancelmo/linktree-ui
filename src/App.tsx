fcd432673a2504c8054958de076179f9760ef8bb
Swetony <swetonyancelmo@gmail.com>
2026-04-01 16:05:58 -0300

    feat: inicializa montagem da pag principal

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,40 @@
+import { FaGithub, FaInstagram, FaLinkedin } from 'react-icons/fa';
+import { Container } from './components/Container';
+import { LinkItem } from './components/LinkItem';
+import { Presentation } from './components/ Presentation';
 
 function App() {
   return (
     <>
-      
+      <div className='flex flex-col items-center justify-center gap-12 mt-9 mb-9'>
+        <Container>
+          <Presentation
+            name='Swetony Ancelmo'
+            profession='Software Developer'
+            imgUrl='/image-me.jpeg'
+          />
+        </Container>
+
+        <Container>
+          <LinkItem
+            icon={<FaGithub className='text-lg' />}
+            name='GitHub'
+            url='https://github.com/swetonyancelmo'
+          />
+          <LinkItem
+            icon={<FaLinkedin className='text-lg' />}
+            name='LinkedIn'
+            url='https://www.linkedin.com/in/swetony-ancelmo/'
+          />
+          <LinkItem
+            icon={<FaInstagram className='text-lg' />}
+            name='Instagram'
+            url='https://www.instagram.com/swetony_/'
+          />
+        </Container>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
